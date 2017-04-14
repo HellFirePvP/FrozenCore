7a0ca3e7341de7d63c9a4d6fa543bade6bd2346c
HellFirePvP <dendorfer.maximilian0@googlemail.com>
2017-04-14 16:33:21 +0200

    <init> added COFH RF api

---
+++ src/main/java/hellfirepvp/frozencore/FrozenCore.java
@@ -0,0 +1,19 @@
+package hellfirepvp.frozencore;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * This class is part of the Astral Sorcery Mod
+ * The complete source code for this mod can be found on github.
+ * Class: FrozenCore
+ * Created by HellFirePvP
+ * Date: 14.04.2017 / 16:26
+ */
+@Mod(modid = FrozenCore.MODID, name = FrozenCore.NAME, version = FrozenCore.VERSION, acceptedMinecraftVersions="[1.10.2]")
+public class FrozenCore {
+
+    public static final String MODID = "frozencore";
+    public static final String NAME = "Frozen Core";
+    public static final String VERSION = "0.1-alpha";
+
+}
